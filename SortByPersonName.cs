e99481e78ad8e185823c9637d9a636b34a66582d
Mohit <94623283+ErElectrical@users.noreply.github.com>
2022-01-15 08:23:08 +0000

    [Add]Mohit Sort by first Name

---
+++ SortByPersonName.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookUser
+{
+    public class DummyContact:Contact,IComparable<DummyContact>
+    {
+        public int CompareTo(DummyContact obj)
+        {
+            return firstName.CompareTo(obj.firstName);
+
+        }
+
+
+    }
+
+    public class sortByName
+    {
+        public static void sortbyFirstName()
+        {
+            Console.WriteLine("before sorting ");
+            foreach(var item in AddressBookMain.addressBook)
+            {
+                Console.WriteLine("First Name : {0} ", item.firstName);
+                Console.WriteLine("Last Name  : {0} ", item.lastName);
+                Console.WriteLine("Phonenumber  : {0} ", item.PhoneNunmber);
+                Console.WriteLine("Address : {0} ", item.address);
+                Console.WriteLine("Email id  : {0} ", item.email);
+                Console.WriteLine("city : {0} ", item.city);
+                Console.WriteLine("state : {0} ", item.state);
+
+
+
+
+
+
+            }
+            DummyContact dum = new DummyContact();
+            AddressBookMain.addressBook.Sort();
+
+            Console.WriteLine("After sorting ");
+            foreach (var item in AddressBookMain.addressBook)
+            {
+                Console.WriteLine("First Name : {0} ", item.firstName);
+                Console.WriteLine("Last Name  : {0} ", item.lastName);
+                Console.WriteLine("Phonenumber  : {0} ", item.PhoneNunmber);
+                Console.WriteLine("Address : {0} ", item.address);
+                Console.WriteLine("Email id  : {0} ", item.email);
+                Console.WriteLine("city : {0} ", item.city);
+                Console.WriteLine("state : {0} ", item.state);
+
+
+
+
+
+
+            }
+
+
+
+        }
+
+    }
+
+
+
+
+}
